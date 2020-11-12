4777cb91d73dd069494c111c6dbb4c76a44d02b2
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-12 23:27:43 +0530

    Added : Functionality to calculate Employee Wage of a day

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -1,11 +1,18 @@
-import math
 import random
 
 class EmployeeWage:
     is_full_time = 1
+    emp_rate_per_hour = 20
+    #raandom.randint returns random value i.e; either 0 and 1
     emp_check = random.randint(0,1)
 
+    #if emp_check = 1 (i.e; employee is present)
+    #if emp_check = 0 (i.e ; employe is absent)
     if emp_check == is_full_time:
-        print("Employee is present")
+        working_hour = 8
     else:
-        print("Employee is Absent")
+        working_hour = 0
+
+    #Calculating Employee Wage Of a Day
+    employee_wage = working_hour * emp_rate_per_hour
+    print("The Employee Wage Of A Day Is : ", employee_wage)
