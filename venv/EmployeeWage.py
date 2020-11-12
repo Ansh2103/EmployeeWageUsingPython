43835a026f6e9d6dae994583b735c92e09fe2267
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-12 23:42:17 +0530

    Added : Functionality to calculate part time and full time wage of an employee

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -2,14 +2,17 @@ import random
 
 class EmployeeWage:
     is_full_time = 1
+    is_part_time = 2
     emp_rate_per_hour = 20
-    #raandom.randint returns random value i.e; either 0 and 1
-    emp_check = random.randint(0,1)
+    #raandom.randint returns random value i.e; either 0, 1 or 2
+    emp_check = random.randint(0,2)
 
     #if emp_check = 1 (i.e; employee is present)
     #if emp_check = 0 (i.e ; employe is absent)
-    if emp_check == is_full_time:
+    if emp_check == is_full_time :
         working_hour = 8
+    elif emp_check == is_part_time :
+        working_hour =4
     else:
         working_hour = 0
 
