f28c178aad9b1afccb6f48d5b00062d2ad08b322
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-13 09:10:58 +0530

    Added : Calculated employee wage using switch case

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -1,21 +1,21 @@
 import random
 
-class EmployeeWage:
-    is_full_time = 1
-    is_part_time = 2
-    emp_rate_per_hour = 20
-    #raandom.randint returns random value i.e; either 0, 1 or 2
-    emp_check = random.randint(0,2)
+is_full_time = 1
+is_part_time = 2
+wage_per_hour = 20
 
-    #if emp_check = 1 (i.e; employee is present)
-    #if emp_check = 0 (i.e ; employe is absent)
-    if emp_check == is_full_time :
-        working_hour = 8
-    elif emp_check == is_part_time :
-        working_hour =4
-    else:
-        working_hour = 0
+#check whether employee is present or absent
+# random.randint returns random value i.e; either 0, 1 or 2
+check_employee = random.randint(0,2)
+def calculate_working_hours(check_employee):
+    switcher = {
+        is_full_time: 8 ,
+        is_part_time: 4 ,
+    }
+    return switcher.get(check_employee , 0)
+#working hour of an employee
+working_hours = calculate_working_hours(check_employee)
 
-    #Calculating Employee Wage Of a Day
-    employee_wage = working_hour * emp_rate_per_hour
-    print("The Employee Wage Of A Day Is : ", employee_wage)
+#calculate employee wage
+employee_wage = working_hours * wage_per_hour
+print ("Employee wage is : " , employee_wage)
