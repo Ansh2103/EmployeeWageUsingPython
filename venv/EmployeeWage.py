485d2416c46540516c0e1ee20be841af09cd1712
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-16 15:17:37 +0530

    Added : Refactor Naming Conventions

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -1,11 +1,11 @@
 import random
 
 #constants and variables
-is_full_time = 1
-is_part_time = 2
-wage_per_hour = 20
-max_working_day = 20
-max_working_hour = 100
+IS_FULL_TIME = 1
+IS_PART_TIME = 2
+WAGE_PER_HOUR = 20
+MAX_WORKING_DAYS = 20
+MAX_WORKING_HOURS = 100
 working_days = 0
 working_hour = 0
 total_working_hour = 0
@@ -15,15 +15,15 @@ monthly_wage = 0
 """ this function to calculate working hour of an employee """
 def calculate_working_hours(check_employee):
     switcher = {
-        is_full_time: 8 ,
-        is_part_time: 4 ,
+        IS_FULL_TIME: 8 ,
+        IS_PART_TIME: 4 ,
     }
     return switcher.get(check_employee , 0)
 
-while working_days <= max_working_day and total_working_hour < max_working_hour:
+while working_days <= MAX_WORKING_DAYS and total_working_hour < MAX_WORKING_HOURS:
     check_employee = random.randint(0, 2)
     working_hour = calculate_working_hours(check_employee)
-    daily_wage = wage_per_hour * working_hour
+    daily_wage = WAGE_PER_HOUR * working_hour
     monthly_wage += daily_wage
     working_days +=1
     total_working_hour += working_hour
