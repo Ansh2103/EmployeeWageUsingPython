b4b7f1d268f04d7503c1a5d2881e8598b5bfae50
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-16 16:04:27 +0530

    Added : Employee Wage Computation Using Class

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -1,30 +1,33 @@
 import random
 
-#constants and variables
-IS_FULL_TIME = 1
-IS_PART_TIME = 2
-WAGE_PER_HOUR = 20
-MAX_WORKING_DAYS = 20
-MAX_WORKING_HOURS = 100
-working_days = 0
-working_hour = 0
-total_working_hour = 0
-daily_wage = 0
-monthly_wage = 0
+class Employee_Wage:
+    WAGE_PER_HOUR = 20
+    MAX_WORKING_DAYS = 20
+    MAX_WORKING_HOURS = 100
 
-""" this function to calculate working hour of an employee """
-def calculate_working_hours(check_employee):
-    switcher = {
-        IS_FULL_TIME: 8 ,
-        IS_PART_TIME: 4 ,
-    }
-    return switcher.get(check_employee , 0)
+    def calculate_working_hours(self,check_employee):
+        IS_FULL_TIME = 1
+        IS_PART_TIME = 2
+        FULL_DAY = 8
+        HALF_DAY = 4
+        switcher = {
+            IS_FULL_TIME: FULL_DAY ,
+            IS_PART_TIME: HALF_DAY ,
+        }
+        return switcher.get(check_employee , 0)
 
-while working_days <= MAX_WORKING_DAYS and total_working_hour < MAX_WORKING_HOURS:
-    check_employee = random.randint(0, 2)
-    working_hour = calculate_working_hours(check_employee)
-    daily_wage = WAGE_PER_HOUR * working_hour
-    monthly_wage += daily_wage
-    working_days +=1
-    total_working_hour += working_hour
-print("The employee's Wage of a month is :", monthly_wage)
+    def calculate_employee_wage(self):
+        working_days = 0
+        total_working_hour = 0
+
+        while working_days <= self.MAX_WORKING_DAYS and total_working_hour < self.MAX_WORKING_DAYS:
+                check_employee = random.randint(0, 2)
+                working_hour = self.calculate_working_hours(check_employee)
+                working_days +=1
+                total_working_hour += working_hour
+
+        total_employee_wage = total_working_hour * self.WAGE_PER_HOUR
+        print("The monthly wage of an employee is  :", total_employee_wage)
+
+employee_wage = Employee_Wage()
+employee_wage.calculate_employee_wage()
