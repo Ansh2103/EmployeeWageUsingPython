3460e2ac976c7115686ea1ec6c8f32ec21940d0a
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-16 07:04:00 +0530

    Added : Functionality to calculate monthly wage of an employee

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -1,21 +1,27 @@
 import random
 
+#constants and variables
 is_full_time = 1
 is_part_time = 2
 wage_per_hour = 20
+max_working_day = 20
+working_days = 0
+working_hour = 0
+daily_wage = 0
+monthly_wage = 0
 
-#check whether employee is present or absent
-# random.randint returns random value i.e; either 0, 1 or 2
-check_employee = random.randint(0,2)
+""" this function to calculate working hour of an employee """
 def calculate_working_hours(check_employee):
     switcher = {
         is_full_time: 8 ,
         is_part_time: 4 ,
     }
     return switcher.get(check_employee , 0)
-#working hour of an employee
-working_hours = calculate_working_hours(check_employee)
 
-#calculate employee wage
-employee_wage = working_hours * wage_per_hour
-print ("Employee wage is : " , employee_wage)
+while working_days <= max_working_day:
+    check_employee = random.randint(0, 2)
+    working_hour = calculate_working_hours(check_employee)
+    daily_wage = wage_per_hour * working_hour
+    monthly_wage += daily_wage
+    working_days +=1
+print("The employee's Wage of a month is :", monthly_wage)
