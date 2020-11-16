dd66fd3be17870f8ff355f7ffd30032ca86a8b1d
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-16 07:24:18 +0530

    Added : Refactor the code and added maximum working hour is 100

--- venv/EmployeeWage.py
+++ venv/EmployeeWage.py
@@ -5,8 +5,10 @@ is_full_time = 1
 is_part_time = 2
 wage_per_hour = 20
 max_working_day = 20
+max_working_hour = 100
 working_days = 0
 working_hour = 0
+total_working_hour = 0
 daily_wage = 0
 monthly_wage = 0
 
@@ -18,10 +20,11 @@ def calculate_working_hours(check_employee):
     }
     return switcher.get(check_employee , 0)
 
-while working_days <= max_working_day:
+while working_days <= max_working_day and total_working_hour < max_working_hour:
     check_employee = random.randint(0, 2)
     working_hour = calculate_working_hours(check_employee)
     daily_wage = wage_per_hour * working_hour
     monthly_wage += daily_wage
     working_days +=1
+    total_working_hour += working_hour
 print("The employee's Wage of a month is :", monthly_wage)
