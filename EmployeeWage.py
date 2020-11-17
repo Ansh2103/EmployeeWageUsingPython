da602dc63dd6e562077e700c975634af290d9648
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-17 22:11:08 +0530

    Refactor : Added __DOC__

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -5,39 +5,40 @@ class Employee_Wage:
     IS_PART_TIME = 2
     FULL_DAY = 8
     HALF_DAY = 4
-    '''
-    Parameters:
-        name = company's name   
-        WAGE_PER_HOUR = the amount getting paid of an employee per hour
-        MAX_WORKING_DAYS : maximum working day limit of an employee in a month
-        MAX_WORKING_HOURS : maximum working hours limit of an employee in a month
-               
-        __init__ : it is the constructor method in python ; 
-                          it is called whenever an object of  class is constucted.
-    '''
+
     def __init__(self,name,WAGE_PER_HOUR,MAX_WORKING_DAYS,MAX_WORKING_HOURS):
+        '''
+            Parameters:
+                name = company's name
+                WAGE_PER_HOUR = the amount getting paid of an employee per hour
+                MAX_WORKING_DAYS : maximum working day limit of an employee in a month
+                MAX_WORKING_HOURS : maximum working hours limit of an employee in a month
+
+                __init__ : it is the constructor method in python ;
+                                  it is called whenever an object of  class is constucted.
+            '''
         self.name = name
         self.WAGE_PER_HOUR = WAGE_PER_HOUR
         self.MAX_WORKING_DAYS = MAX_WORKING_DAYS
         self.MAX_WORKING_HOURS = MAX_WORKING_HOURS
-        '''
-        Checking whether employee is Full_Time, Part Time or Absent
-        if  1: Full time
-            2: Half time
-            3: Absent
-            '''
 
     def calculate_working_hours(self,check_employee):
+        '''
+                Checking whether employee is Full_Time, Part Time or Absent
+                if  1: Full time
+                    2: Half time
+                    3: Absent
+                    '''
         switcher = {
             self.IS_FULL_TIME: self.FULL_DAY,
             self.IS_PART_TIME: self.HALF_DAY,
         }
         return switcher.get(check_employee, 0)
 
-    '''
-        Calculating the monthly wage of an employee 
-        '''
     def calculate_employee_wage(self):
+        '''
+               Calculating the monthly wage of an employee
+        '''
         working_days = 0
         working_hour = 0
         total_working_hour = 0
@@ -50,10 +51,11 @@ class Employee_Wage:
 
         total_employee_wage = total_working_hour * self.WAGE_PER_HOUR
         print("The monthly wage of an employee in the ",self.name,"of a month is : ",total_employee_wage)
-'''
+
+        '''
        creating object of the respective company and returning arguments as
        ("CompanyName",WAGE_PER_HOUR,MAX_WORKING_DAYS,MAX_WORKING_HOURS)
-'''
+        '''
 DMart = Employee_Wage("DMart",20,31,80)
 Reliance = Employee_Wage("Reliance",40,28,90)
 flipcart = Employee_Wage("flipcart",30,30,100)
